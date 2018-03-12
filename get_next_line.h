76f00dfa5992341ef82893be7273e0fd26c90bee
Axel BAL <axbal@e1r7p14.42.fr>
2018-03-12 14:44:09 +0100

    fini

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: axbal <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/08 10:34:34 by axbal             #+#    #+#             */
-/*   Updated: 2018/03/08 10:38:26 by axbal            ###   ########.fr       */
+/*   Updated: 2018/03/12 12:18:29 by axbal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 # define BUFF_SIZE 32
 
-#include <stdlib.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
 int		get_next_line(const int fd, char **line);
 
